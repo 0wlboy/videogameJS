d5b5d4c04173aa6771b25e500e593e26f7fe7f11
0wlboy <35744878+0wlboy@users.noreply.github.com>
2024-11-03 08:31:18 -0400

    update

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ class Personaje{
   }
 
   takeDamage(ataque,defensa){
-    const newDamage = ataque - defensa;
+    let newDamage = Math.floor(Math.random()*ataque) - Math.floor(Math.random()*defensa);
+    if (newDamage < 1){
+      newDamage = 0;
+    }
     console.log(`El ataque cuasa una ${newDamage} despues de que tu oponente se defendiera con ${defensa} puntos de defensa.`)
     this._vida = this._vida - newDamage
     console.log(`Ahora ${this._name} esta a ${this._vida} de puntos de vida`)
@@ -32,9 +35,9 @@ class Personaje{
 class Guerrero extends Personaje{
   constructor(_name,_clase,_vida){
     super(_name,_clase,_vida);
-    this._ataque = 7 + Math.floor(Math.random()*5);
-    this._defensa= 8 + Math.floor(Math.random()*5);
-    this._velocidad =3 + Math.floor(Math.random()*5);
+    this._ataque = 7;
+    this._defensa= 2;
+    this._velocidad =3;
     this._armas=[{
       nombre:'Espada',
       damage: 4,
@@ -54,9 +57,9 @@ class Guerrero extends Personaje{
 class Mago extends Personaje{
   constructor(_name,_clase,_vida){
     super(_name,_clase,_vida);
-    this._ataque = 5 + Math.floor(Math.random()*5);
-    this._defensa= 3 + Math.floor(Math.random()*5);
-    this._velocidad =1 + Math.floor(Math.random()*5);
+    this._ataque = 5;
+    this._defensa= 3;
+    this._velocidad =2;
     this._hechizos=[
       {
         nombre:'Bola de Fuego',
@@ -79,8 +82,8 @@ class Mago extends Personaje{
 class Arquero extends Personaje{
   constructor(_name,_clase,_vida){
     super(_name,_clase,_vida);
-    this._ataque = 8 + Math.floor(Math.random()*5);
-    this._defensa= 1 + Math.floor(Math.random()*5);
+    this._ataque = 8;
+    this._defensa= 1;
     this._velocidad =4 + Math.floor(Math.random()*5);
     this.flechas=10;
     this._damageFlechas = 3 + Math.floor(Math.random()*(5-1)+1)
@@ -98,8 +101,16 @@ class Arquero extends Personaje{
   }
 }
 
+function game(jugadores){
+ while(jugadores.length() > 1){
+  do{}
+  action(jugadores[Math.random()*2],jugadores[Math.random()*2])
+ }
+}
+
 function action(player1,player2){
   let dice = Math.floor(Math.random()*(2-1)+1)
+  let damage = 0;
   console.log(`Miren, ${player1._name} va a atacar a ${player2._name}`)
   if(dice === 1){
     player1.desarmado;
@@ -107,25 +118,35 @@ function action(player1,player2){
   }
   if(dice === 2){
     if(player1.clase === "Guerrero"){
-      player2.takeDamage(player1.UsarArma,player2._defensa)
+      damage = player1.UsarArma
+      player2.takeDamage(damage,player2._defensa)
     }else if(player1.clase === "Mago"){
-      player2.takeDamage(player1.lanzarHechizo,player2._defensa)
+      damage = player1.lanzarHechizo
+      player2.takeDamage(damage,player2._defensa)
     }else if(player1.clase === "Arquero"){
-      player2.takeDamage(player1.dispararFlecha,player2._defensa)
+      damage = player1.dispararFlecha
+      player2.takeDamage(damage,player2._defensa)
     }
   }
 }
 
+function round(players){
+  let orden;
+  for(let x = 0; x<4;x++){
+    orden = players.sort((a,b)=>{
+      return ( Math.floor(Math.random()* b._velocidad)) - (Math.floor(Math.random() * a._velocidad));
+    })
+  }
+  return orden;
+}
+
 let jugadores = [];
 
-jugadores.push(new Guerrero('Grog','Guerrero'));
+jugadores.push(new Guerrero('Guts','Guerrero'));
 jugadores.push(new Guerrero('Cloud','Guerrero'));
 jugadores.push(new Mago('Aerith','Mago'));
 jugadores.push(new Mago('Zatanna','Mago'));
 jugadores.push(new Arquero('Katniss','Arquero'));
 
-let orden = jugadores.sort((a,b)=>{
-  return b._velocidad - a._velocidad;
-})
+let orden = round(jugadores)
 
-action(orden[0],orden[1])
